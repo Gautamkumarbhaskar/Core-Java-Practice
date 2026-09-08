fc5837451e28bec0de351025806cb83b6a9964e3
Gautam kumar bhaskar <krgautam258000_email@gmail.com>
2026-09-08 17:16:45 +0530

    Add Addition Program

---
+++ src/Addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value for a");
+        int a = sc.nextInt();
+        System.out.println("Enter value for b");
+        int b = sc.nextInt();
+
+        int c = a+b;
+        System.out.println("addition =" + c);
+        sc.close();
+
+
+
+
+
+    }
+
+}
